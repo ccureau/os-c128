cb675385deb816099a5c59498ba315dd1feccbbf
Chris Cureau <christopher.cureau@ge.com>
2017-01-24 15:51:12 -0600

    preliminary cursor support

--- video/main.c
+++ video/main.c
@@ -1,9 +1,16 @@
+#define LOWBYTE(v)   ((unsigned char) (v))
+#define HIGHBYTE(v)  ((unsigned char) (((unsigned int) (v)) >> 8))
+
 void vdc_write_memory(unsigned char c);
 void vdc_write_reg(unsigned char reg, unsigned char val);
 void vdc_clear_screen();
 unsigned char vdc_read_memory();
 unsigned char vdc_read_reg(unsigned char reg);
+void vdc_set_cursor();
 void setup(void);
+
+extern unsigned int vdc_cursor_pos;
+extern unsigned int vdc_attrib_pos;
 
 /* TODO: screen characters are different than petscii characters.
  * Clear bit 6 for regular characters, bit 5 for above 127.
@@ -14,14 +21,30 @@ void strout(char *s1){
   unsigned char *ptr = s1;
   while (*ptr != '\0') {
     vdc_write_memory(*(ptr++) & 0x3f);
+    ++vdc_cursor_pos;
+    ++vdc_attrib_pos;
+    vdc_set_cursor();
   }
+}
+
+unsigned int calc_mem_pos(unsigned char row, unsigned char col) {
+  return (row*80)+col;
+}
+
+void vdc_gotoxy(unsigned char row, unsigned char col) {
+  unsigned int pos = calc_mem_pos(row, col);
+  vdc_cursor_pos = pos;
+  vdc_attrib_pos = pos + 0x800;
+  vdc_write_reg(0x12, HIGHBYTE(vdc_cursor_pos));
+  vdc_write_reg(0x13, LOWBYTE(vdc_cursor_pos));
 }
 
 unsigned char main() {
     vdc_clear_screen();
-    vdc_write_reg(0x12, 0x00);
-    vdc_write_reg(0x13, 0x00);
+    //vdc_write_reg(0x12, 0x00);
+    //vdc_write_reg(0x13, 0x00);
 
+    vdc_gotoxy(15,10);
     strout("hi there!");
-    return 0;
+    while(1) {}
 }
--- video/vdc.s
+++ video/vdc.s
@@ -1,5 +1,6 @@
     .export    _vdc_write_memory, _vdc_write_reg, _vdc_read_memory, _vdc_read_reg
-    .export    _vdc_clear_screen
+    .export    _vdc_clear_screen, _vdc_set_cursor
+    .export    _vdc_cursor_pos, _vdc_attrib_pos
     .import    popa, pusha, popax
     .importzp  tmp2, tmp3
 
@@ -7,26 +8,41 @@
 .define   VDC_CONTROL       $D600
 .define   VDC_DATA          $D601
 
-.define   VDC_REG_MEMHIGH   0x12
-.define   VDC_REG_MEMLOW    0x13
+.define   VDC_REG_CURSHIGH    $0E
+.define   VDC_REG_CURSLOW     $0F
+.define   VDC_REG_MEMHIGH     $12
+.define   VDC_REG_MEMLOW      $13
 
 .define	  VDC_MEM_RW        $1F
 
 ; zero page variables
 .segment	"BSS"
-vdc_tmp_high:
-  .res 1
-vdc_tmp_low:
-  .res 1
+vdc_tmp:
+  .res 2
+_vdc_cursor_pos:
+  .res 2
+_vdc_attrib_pos:
+  .res 2
 
 
 ; code
 .segment	"CODE"
 
+_vdc_set_cursor:
+    lda   #VDC_REG_CURSHIGH
+    jsr   pusha
+    lda   _vdc_cursor_pos+1
+    jsr   _vdc_write_reg
+    lda   #VDC_REG_CURSLOW
+    jsr   pusha
+    lda   _vdc_cursor_pos
+    jsr   _vdc_write_reg
+    rts
+
 _vdc_clear_screen:
     lda   #$00
-    sta   vdc_tmp_low
-    sta   vdc_tmp_high
+    sta   vdc_tmp
+    sta   vdc_tmp+1
     lda   #$20
     sta   tmp2
     lda   #$08
@@ -34,9 +50,9 @@ _vdc_clear_screen:
     jsr   vdc_clear_loop
 
     lda   #$00
-    sta   vdc_tmp_low
+    sta   vdc_tmp
     lda   #$08
-    sta   vdc_tmp_high
+    sta   vdc_tmp+1
     lda   #$87              ; attribute
     sta   tmp2
     lda   #$08
@@ -48,12 +64,12 @@ _vdc_clear_screen:
 vdc_clear_loop:
     lda   #$12
     jsr   pusha
-    lda   vdc_tmp_high
+    lda   vdc_tmp+1
     jsr   _vdc_write_reg
 
     lda   #$13
     jsr   pusha
-    lda   vdc_tmp_low
+    lda   vdc_tmp
     jsr   _vdc_write_reg
 
     lda   tmp2
@@ -72,11 +88,11 @@ vdc_clear_loop:
 
     lda   #$12
     jsr   _vdc_read_reg
-    sta   vdc_tmp_high
+    sta   vdc_tmp+1
 
     lda   #$13
     jsr   _vdc_read_reg
-    sta   vdc_tmp_low
+    sta   vdc_tmp
 
     dec   tmp3
     bne   vdc_clear_loop
